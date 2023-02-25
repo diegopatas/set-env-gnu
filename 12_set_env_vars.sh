5188fdf72793febc65ea392691055201b71060bc
Diego Diniz <ddiniz@student.42sp.org.br>
2023-02-24 22:31:16 -0500

    feat: add git global settings

--- 12_set_env_vars.sh
+++ 12_set_env_vars.sh
@@ -3,3 +3,8 @@
 export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/set-env-gnu
 export PATH=$PATH:$HOME/main/bin
+
+# GIT
+git config --global user.email "ddiniz@student.42sp.org.br"
+git config --global user.name "Diego Diniz"
+git config --global core.editor vim
