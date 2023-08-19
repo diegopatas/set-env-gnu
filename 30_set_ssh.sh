379499d6cf3b31eafd080e7ab44d5694bf7bbee6
Diego Diniz <ddiniz@student.42sp.org.br>
2023-08-19 16:46:03 -0400

    Feat: add tmux script

--- 30_set_ssh.sh
+++ 30_set_ssh.sh
@@ -7,3 +7,6 @@ ssh-keygen -t ed25519 -C "ddiniz@student.42sp.org.br"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
+
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/42vogsphere && clear
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github && clear
---
+++ 32_set_tmux_plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash bash
+
+~/.tmux/plugins/tmux-resurrect/resurrect.tmux
+~/.tmux/plugins/tmux-sensible/sensible.tmux
+~/.tmux/plugins/tmux-themepack/themepack.tmux
+~/.tmux/plugins/tmux-yank/yank.tmux
+~/.tmux/plugins/tpm/tpm
